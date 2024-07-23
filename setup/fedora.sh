ef7068014b8ff49b65e8a1275bb5fb2eda57f573
answer35 <rbohanne@proton.me>
2024-07-23 23:46:36 +0200

    Update fedora.sh

--- setup/fedora.sh
+++ setup/fedora.sh
@@ -23,23 +23,23 @@ sudo dnf install \
     glibc-static \
     libstdc++-static \
     libX11-devel \
+    libxcrypt-compat \
     make \
     mesa-libGL-devel \
     ncurses-devel \
     openssl \
+    openssl-devel \
     patch \
     zlib-devel \
     ncurses-devel.i686 \
     readline-devel.i686 \
-    zlib-devel.i686 \
-    libX11-devel.i686 \
     mesa-libGL-devel.i686 \
     glibc-devel.i686 \
     libstdc++.i686 \
     libXrandr.i686 \
     zip \
     perl-Digest-SHA \
-    python2 \
+    python3 \
     wget \
     lzop \
     openssl-devel \
@@ -48,8 +48,7 @@ sudo dnf install \
     schedtool \
     lzip \
     vboot-utils \
-    vim \
-    libxcrypt-compat
+    vim
 
 # The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
